feb796ae2979b7d4b1ef3aba94588d23012026c3
Zhiheng Xu <xeric.2002@gmail.com>
2021-10-25 19:53:51 -0400

    Refactor file writing into with block
    This closes the resources as soon as possible

--- generate.py
+++ generate.py
@@ -6,16 +6,18 @@ names = sys.argv[1:]
 
 with open('templates.json') as f:
     templates = json.load(f)
-f = open('generated.gitignore', 'w')
 
-for name in names:
-    # `header` is a "flowerbox" comment to separate the different types
-    header = 30 * "#" + "\n"
-    header += "# " + name + (27 - len(name)) * " " + "#\n"
-    header += 30 * "#" + "\n\n"
-    f.write(header)
-    # Appends the contents of the .gitignore template
-    r = requests.get(
-        "https://raw.githubusercontent.com/github/gitignore/master/" + templates[name])
-    f.write(r.text)
-    f.write('\n')
+with open('generated.gitignore', 'w') as f:
+    for name in names:
+        # `header` is a "flowerbox" comment to separate the different types
+        header = 30 * "#" + "\n"
+        header += "# " + name + (27 - len(name)) * " " + "#\n"
+        header += 30 * "#" + "\n\n"
+        f.write(header)
+        # Appends the contents of the .gitignore template
+        r = requests.get(
+            "https://raw.githubusercontent.com/github/gitignore/master/"
+            + templates[name]
+        )
+        f.write(r.text)
+        f.write('\n')
